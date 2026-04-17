ed787edb8edc5f18e334d444e4e0875de6afaec2
frltrippin <trippinbuilder@gmail.com>
2026-04-16 18:25:03 -0700

    update

--- api/checkout.js
+++ api/checkout.js
@@ -3,23 +3,19 @@ const Stripe = require('stripe');
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        
-        if (req.method === 'OPTIONS') {
-            return res.status(200).end();
-        }
-        
-        return res.status(405).json({ error: 'Method not allowed' });
-    }
-
-    // CORS headers
+module.exports = async (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method !== 'POST') {
+        if (req.method === 'OPTIONS') {
+            return res.status(200).end();
+        }
+
+        return res.status(405).json({ error: 'Method not allowed' });
+    }
 
     const cart = Array.isArray(req.body.cart) ? req.body.cart : [];
     
